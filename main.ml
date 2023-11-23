45fe12f3521c0b13212ae88431a3467442aaea73
JanMaciuk <272664@student.pwr.edu.pl>
2023-11-23 15:09:37 -0500

    Task 3 complete

--- main.ml
+++ main.ml
@@ -1,6 +1,6 @@
 
 
-module Point3DModule = struct
+module Point_3D_module = struct
   type point3D = { x: int; y: int; z: int }
 
   let getDistance a b =
@@ -10,19 +10,19 @@ module Point3DModule = struct
 end
 
 module Line = struct
-  type line = { a: Point3DModule.point3D; b: Point3DModule.point3D }
+  type line = { a: Point_3D_module.point3D; b: Point_3D_module.point3D }
 
-  let createLine a b = { a = a; b = b }
+  let create_line a b = { a = a; b = b }
 
-  let getLength line = Point3DModule.getDistance line.a line.b
+  let get_length line = Point_3D_module.getDistance line.a line.b
 end
 
-module BinaryTree = struct
+module Binary_tree = struct
   type tree =
     | Empty
     | Node of int * tree * tree   (*value, left child, right child*)
 
-  let rec add_node value = function
+  let rec add_node value = function (* = function is shorter way of writing match argument with*)
     | Empty -> Node (value, Empty, Empty)
     | Node (v, left, right) ->
         if value < v then Node (v, add_node value left, right) (* call adding to left subtree *)
@@ -50,5 +50,32 @@ let rec delete_node value = function
     | Node (v, Empty, _) -> v (* if there are no smaller values, return current value*)
     | Node (_, left, _) -> find_min left (* keep going to the left subtree, while we can *)
 
- (*TODO: walk the tree*)
+    let rec inorder_walk = function (* = function is shorter way of writing match argument with*)
+    | Empty -> []
+    | Node (v, left, right) -> inorder_walk left @ [v] @ inorder_walk right
+
+  let rec preorder_walk = function
+    | Empty -> []
+    | Node (v, left, right) -> [v] @ preorder_walk left @ preorder_walk right
+
+  let rec postorder_walk = function
+    | Empty -> []
+    | Node (v, left, right) -> postorder_walk left @ postorder_walk right @ [v]
 end
+
+  (*test tasks 1,2 and 3*)
+  let point1 = Point_3D_module.createPoint 1 2 3
+  let point2 = Point_3D_module.createPoint 4 5 6
+
+  let line = Line.create_line point1 point2 
+
+  let length = Line.get_length line 
+  let _ = print_endline "Length of the line:"
+  let _ = print_endline (string_of_float length)
+
+  (* Create a binary tree *)
+  let tree = Binary_tree.Node(1, Binary_tree.Node(2, Binary_tree.Empty, Binary_tree.Empty), Binary_tree.Node(3, Binary_tree.Empty, Binary_tree.Empty))
+  
+  let tree_elements = Binary_tree.preorder_walk tree
+  let _ = print_endline "COntents of the tree:"
+  let _ = print_endline (String.concat " " (List.map string_of_int tree_elements))
